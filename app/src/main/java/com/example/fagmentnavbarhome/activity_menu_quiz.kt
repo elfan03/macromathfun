de995ada6f7bb5f79199de62c6bc7d41bfc67b6f
Agum Pratama <pratamaagum15@gmail.com>
2023-10-19 00:10:27 +0700

    quiz

--- app/src/main/java/com/example/fagmentnavbarhome/activity_menu_quiz.kt
+++ app/src/main/java/com/example/fagmentnavbarhome/activity_menu_quiz.kt
@@ -1,11 +1,25 @@
 package com.example.fagmentnavbarhome
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class activity_menu_quiz : AppCompatActivity() {
+class activity_menu_quiz : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_quiz)
+        val btnspldv: Button = findViewById(R.id.btn_spldv)
+        btnspldv.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btn_spldv -> {
+                val intent = Intent(this@activity_menu_quiz, activity_Quiz_SPLDV1::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
