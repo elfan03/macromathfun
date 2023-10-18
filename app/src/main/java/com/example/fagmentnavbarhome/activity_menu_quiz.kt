bacf15e828e3d8e0c3e17817f9bb2e440400188a
Agum Pratama <pratamaagum15@gmail.com>
2023-10-19 00:32:07 +0700

    update

--- app/src/main/java/com/example/fagmentnavbarhome/activity_menu_quiz.kt
+++ app/src/main/java/com/example/fagmentnavbarhome/activity_menu_quiz.kt
@@ -10,14 +10,29 @@ class activity_menu_quiz : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_quiz)
+
         val btnspldv: Button = findViewById(R.id.btn_spldv)
         btnspldv.setOnClickListener(this)
+
+        val btnaljabar: Button = findViewById(R.id.btn_aljabar)
+        btnaljabar.setOnClickListener(this)
+
+        val btnlinear: Button = findViewById(R.id.btn_linear)
+        btnlinear.setOnClickListener(this)
     }
 
     override fun onClick(v: View) {
         when (v.id) {
             R.id.btn_spldv -> {
                 val intent = Intent(this@activity_menu_quiz, activity_Quiz_SPLDV1::class.java)
+                startActivity(intent)
+            }
+            R.id.btn_aljabar -> {
+                val intent = Intent(this@activity_menu_quiz, activity_Quiz_Aljabar::class.java)
+                startActivity(intent)
+            }
+            R.id.btn_linear -> {
+                val intent = Intent(this@activity_menu_quiz, activity_Quiz_FLinear1::class.java)
                 startActivity(intent)
             }
         }
